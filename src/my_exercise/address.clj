1ccba36d401ea2dd9ffed272cbe910068cd0aea5
Anonymous Applicant <anonymous@example.com>
2018-03-30 12:37:37 -0600

    Add documentation note with error for accessing state in hashmap

--- src/my_exercise/address.clj
+++ src/my_exercise/address.clj
@@ -1,10 +1,8 @@
 (ns my-exercise.address)
 
 
-; (defn builder [params])
-
-
+; lower-casing the state abbreviation and appending it to ocd-division/country:us/state:
+; ** Error accessing the state from the params hashmap
 (defn state-ocd [params]
   (let [state (get params :state)])
   (str "ocd-division/country:us/state:" (clojure.string/lower-case state)))
-; lower-casing the state abbreviation and appending it to ocd-division/country:us/state:
